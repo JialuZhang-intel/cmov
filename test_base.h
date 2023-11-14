790f5ee63ce57813c2bd530c3ffc054d2810700a
JialuZhang-intel <jialu.zhang@intel.com>
2023-11-14 16:42:12 +0800

    Update test case.

--- test_base.h
+++ test_base.h
@@ -3,9 +3,10 @@ void check_eq(int expected, int value, const char* msg);
 int FUNC_NAME(int const* A, int const* B, int const n, int const iters)
 {
     int i, j;
-    int res;
+    int res, res2;
 
     res = 0;
+    res2 = 0;
     for(i = 0; i < iters; ++i) {
         for(j = 0; j < n; ++j) {
             #ifdef CHECK
@@ -49,11 +50,15 @@ int FUNC_NAME(int const* A, int const* B, int const n, int const iters)
             #elif ASM_CMOV_MEM_WITH_USE
                 asm ("cmp %[A_j], 0\n\t"
                     "cmovne %[res], %[B_j]\n\t"
-                    "imul %[res], %[B_j]\n\t"
-                    "imul %[res], %[B_j]\n\t"
-                    "imul %[res], %[B_j]\n\t"
-                    "imul %[res], %[B_j]"
-                    : [res] "+r"(res)
+                    "add %[res2] ,4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2] ,4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2] ,4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2], 4"
+                    : [res] "+r"(res), [res2] "+r"(res2)
                     : [A_j] "m"(A[j]), [B_j] "m"(B[j])
                     : "cc");
 
@@ -66,11 +71,15 @@ int FUNC_NAME(int const* A, int const* B, int const n, int const iters)
                         : done);
                 res = B[j];
                 done:
-                asm ("imul %[res], %[B_j]\n\t"
-                    "imul %[res], %[B_j]\n\t"
-                    "imul %[res], %[B_j]\n\t"
-                    "imul %[res], %[B_j]"
-                    : [res] "+r"(res)
+                asm ("add %[res2], 4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2] ,4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2] ,4\n\t"
+                    "add %[res2], 4\n\t"
+                    "add %[res2], 4"
+                    : [res2] "+r"(res2)
                     : [B_j] "m"(B[j]));
 
             #else
